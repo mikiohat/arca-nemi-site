a702e34e0aeaa0df86fcfe2e559fabaead1e9314
mikiohat <mikiohat@gmail.com>
2025-11-14 17:29:43 +0900

    .gitignore/.gitignore astro.config.mjs/astro.config.mjs common/Analytics.astro common/BaseHead.astro common/Footer.astro common/Header.astro common/HeaderLink.astro talk/TalkRow.astro layouts/BaseLayout.astro pages/index.astro filtering-feelings/index.astro nemi-and-index/index.astro rollback-black-history/index.astro sql/index.astro try/index.astro tsconfig.json/tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
 import mdx from '@astrojs/mdx';
 
 // 必要なら sitemap は後で戻せる
@@ -9,5 +10,20 @@ export default defineConfig({
   site: 'https://arcanemi.com', // ← 仮のURLに変更
   integrations: [mdx()],
   // integrations: [mdx(), sitemap()],
-  server: { host: true }
+  server: { host: true },
+
+  vite: { 
+      resolve: { 
+          alias: { 
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@common': fileURLToPath(new URL('./src/components/common', import.meta.url)),
+            '@playground': fileURLToPath(new URL('./src/components/playground', import.meta.url)),
+            '@talk': fileURLToPath(new URL('./src/components/talk', import.meta.url)),
+            '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+            '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),          
+          },
+      },
+  },
+
 });
+
